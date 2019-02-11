69a190bd4070a86a90558964d4603211c90d87b4
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-11 13:43:07 +0100

    cleaning

--- src/components/companies/partners/partner.jsx
+++ src/components/companies/partners/partner.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
 import ExternalLink from '../../ui/external-link';
 
-class Partner extends React.Component {
-  /*= ({ partner, small = "small-3" }) => {
+const Partner = ({ partner, className = '' }) => {
   const { logo, name, url } = partner;
   let { path } = logo;
-  const Logo = async () => {
-    await import("../../../assets/img/" + path).then(img => console.log(img));
-  };*/
-  constructor(props) {
-    super(props);
-    this.state = { logo: null };
-  }
-  async componentDidMount() {
-    let { path } = this.props.partner.logo;
-    const logo = await import('../../../assets/img/' + path);
-    this.setState({ logo: logo.default });
-  }
-  render() {
-    const { partner, small = 'small-3', className = '' } = this.props;
-    let classNames = ['medium-auto cell', small, className].join(' ');
-    return (
-      <div className={classNames}>
-        <ExternalLink href={partner.url} className="vertical-align-container">
-          <img src={this.state.logo} alt={partner.name} />
-        </ExternalLink>
-      </div>
-    );
-  }
-}
+
+  let classNames = ['small-3 medium-2 cell', className].join(' ');
+  return (
+    <div className={classNames}>
+      <ExternalLink href={url} className="vertical-align-container">
+        <img src={path} alt={name} />
+      </ExternalLink>
+    </div>
+  );
+};
 
 export default Partner;
