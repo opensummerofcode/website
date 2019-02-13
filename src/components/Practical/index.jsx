330da689d0ede63b9fb24ed3b7392d1cac30d3d7
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-13 11:12:41 +0100

    Extract When content inside a standalone component

--- src/components/Practical/index.jsx
+++ src/components/Practical/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ExternalLink from '../UI/ExternalLink'; */
 import PracticalHeader from './PracticalHeader';
 import PracticalTabs from './PracticalTabs';
+import When from './When';
 
 const Practical = () => {
   return (
@@ -11,30 +12,7 @@ const Practical = () => {
       <PracticalTabs />
 
       <div className="tabs-content c-faq-answers" data-tabs-content="example-tabs">
-        <div className="tabs-panel is-active" id="when">
-          <div className="grid-x grid-padding-x u-margin-large--tb">
-            <div className="small-12 cell">
-              <h3 className="h5">
-                2 July &mdash; 26 July{' '}
-                <span className="subheader">From Monday to Thursday, 9AM to 5PM</span>
-              </h3>
-              <p>You don't have to work on Fridays or weekends!</p>
-              <div className="loading loading--calendar">
-                {/* <!-- <iframe class="calendar" src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=mietclaes.com_h1rs31a79809r742e5lpdpegbg%40group.calendar.google.com&amp;color=%2342104A&amp;ctz=Europe%2FBrussels&dates=20180702%2F20180726" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe> --> */}
-                <iframe
-                  src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=en_GB&amp;bgcolor=%23ffffff&amp;src=hl66h7n0e50q8qf7el9oq8hbsg%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=Europe%2FBrussels&dates=20180702%2F20180726"
-                  style={{ borderWidth: 0 }}
-                  width="100%"
-                  height="800"
-                  frameBorder="0"
-                  scrolling="no"
-                  title="Schedule"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <When />
         <div className="tabs-panel" id="where">
           <div className="grid-x grid-padding-x u-margin-large--tb">
             <div className="small-12 medium-6 large-5 cell">
