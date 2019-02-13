16b99e2914a217a5ad4a617dfff5570adf90305b
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-13 10:41:41 +0100

    Add PracticalTabs

--- src/components/Practical/index.jsx
+++ src/components/Practical/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 /* import { Link } from 'react-router-dom';
 import ExternalLink from '../UI/ExternalLink'; */
 import PracticalHeader from './PracticalHeader';
+import PracticalTabs from './PracticalTabs';
 
 const Practical = () => {
   return (
     <React.Fragment>
       <PracticalHeader />
+      <PracticalTabs />
     </React.Fragment>
   );
 };
