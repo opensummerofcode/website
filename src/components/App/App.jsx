7d8cd048753f8ced503f634c29053ff4f88c9d50
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-14 13:17:51 +0100

    Add path for companies page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import $ from 'jquery';
 import 'foundation-sites';
 import MainLayout from '../Layouts/Main';
+import LayoutWithProjects from '../Layouts/Alternatives/LayoutWithProjects';
 import PublicRoute from '../PublicRoute';
 import Home from '../Home';
 import Practical from '../Practical';
 import Students from '../Students';
+import Companies from '../Companies';
 import Coaches from '../Coaches';
 
 class App extends React.Component {
@@ -22,6 +24,7 @@ class App extends React.Component {
           <PublicRoute path="/practical" layout={MainLayout} component={Practical} />
           <PublicRoute path="/students" layout={MainLayout} component={Students} />
           <PublicRoute path="/coaches" layout={MainLayout} component={Coaches} />
+          <PublicRoute path="/companies" layout={LayoutWithProjects} component={Companies} />
         </Switch>
       </Router>
     );
