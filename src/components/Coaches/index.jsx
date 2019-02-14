36388d02b8d480adc3cacf4aef930d4983a64eb5
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-14 14:36:34 +0100

    Add testimonials section

--- src/components/Coaches/index.jsx
+++ src/components/Coaches/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Info from './Info';
+import Testimonials from './Testimonials';
 
 const Coaches = () => {
   return (
     <React.Fragment>
       <Header />
       <Info />
+      <Testimonials />
     </React.Fragment>
   );
 };
