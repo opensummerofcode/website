1d75df143040e858287f9f03745717ea2ea94e61
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-14 09:50:54 +0100

    Add info section

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Info from './Info';
 
 const Students = () => {
   return (
     <React.Fragment>
       <Header />
+      <Info />
     </React.Fragment>
   );
 };
