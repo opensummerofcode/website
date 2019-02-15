5d1f004e3af30cb89c6a9ddd806618e72079c7a4
Michiel Leyman <michiel.leyman@gmail.com>
2019-02-15 14:31:03 +0100

    Remove fragment

--- src/components/Practical/PracticalHeader.jsx
+++ src/components/Practical/PracticalHeader.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 
 const PracticalHeader = () => {
   return (
-    <React.Fragment>
-      {/* <!-- header --> */}
-      <header className="hero-detail u-padding-x-large--tb">
-        <div className="grid-x grid-padding-x">
-          <div className="medium-4 medium-offset-0 large-4 cell">
-            <h1 className="hero-head">Frequently asked questions</h1>
-          </div>
-
-          <div className="medium-6 medium-offset-0 large-3 cell large-offset-1">
-            <p className="hero-text">There&apos;s no such thing as a dumb&nbsp;question.</p>
-            <div className="osoc-float-element-o-small" />
-          </div>
+    <header className="hero-detail u-padding-x-large--tb">
+      <div className="grid-x grid-padding-x">
+        <div className="medium-4 medium-offset-0 large-4 cell">
+          <h1 className="hero-head">Frequently asked questions</h1>
         </div>
-      </header>
-    </React.Fragment>
+
+        <div className="medium-6 medium-offset-0 large-3 cell large-offset-1">
+          <p className="hero-text">There&apos;s no such thing as a dumb&nbsp;question.</p>
+          <div className="osoc-float-element-o-small" />
+        </div>
+      </div>
+    </header>
   );
 };
 
