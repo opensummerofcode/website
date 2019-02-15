40fc065fa76d1a170cc0f3db456f1aed39cd966b
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-15 15:36:26 +0100

    Add coaches header

---
+++ src/components/Editions/CoachesHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const CoachesHeader = () => {
+  return (
+    <header className="hero-detail u-padding-x-large--tb border-top-medium-gray">
+      <div className="grid-x grid-padding-x">
+        <div className="medium-12 medium-offset-0 large-4 cell">
+          <h1 className="hero-head">
+            This year&apos;s coaches
+            <div className="osoc-float-element-s" />
+          </h1>
+        </div>
+
+        <div className="small-6 medium-offset-0 large-3 cell large-offset-1">
+          <p className="hero-text">
+            developers
+            <br /> designers
+            <br /> marketers
+            <br /> business&nbsp;analysts
+            <br /> data scientists
+            <br /> &amp; other digital skills
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default CoachesHeader;
--- src/components/Editions/index.jsx
+++ src/components/Editions/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProjectsHeader from './ProjectsHeader';
 import StudentsHeader from './StudentsHeader';
+import CoachesHeader from './CoachesHeader';
 
 const YearEdition = () => {
   return (
     <React.Fragment>
       <ProjectsHeader />
       <StudentsHeader />
+      <CoachesHeader />
     </React.Fragment>
   );
 };
