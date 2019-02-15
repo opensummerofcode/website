eb774bdc5ad6c8e9949de34e78ec65d0c21eedf2
Michiel Leyman <michiel.leyman@gmail.com>
2019-02-15 11:49:21 +0100

    Remove fragments and comments for coaches

--- src/components/Coaches/Header.jsx
+++ src/components/Coaches/Header.jsx
@@ -2,31 +2,28 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <React.Fragment>
-      {/* <!-- header --> */}
-      <header className="hero-detail u-padding-x-large--tb">
-        <div className="grid-x grid-padding-x">
-          <div className="medium-4 medium-offset-0 large-4 cell">
-            <h1 className="hero-head">
-              Osoc
-              <br /> for coaches
-            </h1>
-          </div>
-
-          <div className="medium-6 medium-offset-0 large-5 cell large-offset-1">
-            <p className="hero-text">
-              developers
-              <br /> designers
-              <br /> marketers
-              <br /> business&nbsp;analysts
-              <br /> data scientists
-              <br /> &amp; other digital skills
-            </p>
-            <div className="osoc-float-element-s" />
-          </div>
+    <header className="hero-detail u-padding-x-large--tb">
+      <div className="grid-x grid-padding-x">
+        <div className="medium-4 medium-offset-0 large-4 cell">
+          <h1 className="hero-head">
+            Osoc
+            <br /> for coaches
+          </h1>
         </div>
-      </header>
-    </React.Fragment>
+
+        <div className="medium-6 medium-offset-0 large-5 cell large-offset-1">
+          <p className="hero-text">
+            developers
+            <br /> designers
+            <br /> marketers
+            <br /> business&nbsp;analysts
+            <br /> data scientists
+            <br /> &amp; other digital skills
+          </p>
+          <div className="osoc-float-element-s" />
+        </div>
+      </div>
+    </header>
   );
 };
 
