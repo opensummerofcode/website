e591e909844eab2b5f1bcd3c5b67fc77e6dc7299
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-15 15:23:46 +0100

    Add students header

--- src/components/Editions/index.jsx
+++ src/components/Editions/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProjectsHeader from './ProjectsHeader';
+import StudentsHeader from './StudentsHeader';
 
 const YearEdition = () => {
   return (
     <React.Fragment>
       <ProjectsHeader />
+      <StudentsHeader />
     </React.Fragment>
   );
 };
