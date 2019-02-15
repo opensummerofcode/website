c57f69d23b97dcd279f9d4d1d1d1a4870012046d
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-15 14:20:33 +0100

    Add gallery component

---
+++ src/components/Editions/Gallery.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Gallery = ({ className, children }) => {
+  const classNames = ['u-padding-x-large--tb', className].join(' ');
+  return (
+    <div className={classNames}>
+      <div className="grid-x grid-padding-x">{children}</div>
+    </div>
+  );
+};
+
+Gallery.defaultProps = {
+  className: ''
+};
+
+Gallery.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+export default Gallery;
