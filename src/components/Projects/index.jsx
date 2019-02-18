1870f660d84aafb0a9ec9fe263456e3ce7e2fa26
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-18 12:30:36 +0100

    Add partners section

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import Header from './Header';
 import Team from './Team';
+import Partners from './Partners';
 
 const Project = () => {
   return (
     <React.Fragment>
       <Header />
       <Team />
+      <Partners />
     </React.Fragment>
   );
 };
