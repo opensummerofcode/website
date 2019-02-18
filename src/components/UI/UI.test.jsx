25db438bacb16b641a1e2306812d0d055baf59e0
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-18 17:04:40 +0100

    Add tests

--- src/components/UI/UI.test.jsx
+++ src/components/UI/UI.test.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ExternalLink from './ExternalLink';
 
+configure({ adapter: new Adapter() });
 describe('ExternalLink', () => {
+  const externalLink = <ExternalLink href="https://2018.summerofcode.be/">osoc2018</ExternalLink>;
+  const ELShallow = shallow(externalLink);
   it('renders correctly', () => {
-    const tree = renderer
-      .create(<ExternalLink href="https://2018.summerofcode.be/">osoc2018</ExternalLink>)
-      .toJSON();
+    const tree = renderer.create(externalLink).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders as <a>', () => {
+    expect(ELShallow.is('a')).toBeTruthy();
+  });
+
+  it('checks the value of href', () => {
+    expect(ELShallow.prop('href')).toEqual('https://2018.summerofcode.be/');
+  });
+
+  it('renders children', () => {
+    expect(ELShallow.text()).toEqual('osoc2018');
   });
 });
