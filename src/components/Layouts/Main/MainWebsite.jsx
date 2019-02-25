3abe850ce1f61f398348a6038d37d98c630ffd61
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-25 13:37:38 +0100

    Update MainWebsite

--- src/components/Layouts/Main/MainWebsite.jsx
+++ src/components/Layouts/Main/MainWebsite.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from '../../Common/Navigation';
 import Footer from '../../Common/Footer';
-import Partners from '../../Companies/Partners';
 
-const MainLayout = ({ component: Component }) => {
+const MainWebsite = ({ sub: SubLayout, component: Component }) => {
+  const Content = SubLayout || Component;
   return (
     <React.Fragment>
       <Navigation />
-      <Component />
-      <Partners />
+      <Content component={Component} />
       <Footer />
     </React.Fragment>
   );
 };
 
-MainLayout.propTypes = {
-  component: PropTypes.func.isRequired
+MainWebsite.propTypes = {
+  component: PropTypes.func.isRequired,
+  sub: PropTypes.func.isRequired
 };
 
-export default MainLayout;
+export default MainWebsite;
