f37a80a8d205cf78f43af3ca13ffee7f67e08b9c
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-26 11:22:08 +0100

    Add Button SuccessButton & SecondaryButton

--- src/components/UI/Buttons.jsx
+++ src/components/UI/Buttons.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const buttonDefaulProps = {
+  className: ''
+};
+
+const buttonType = {
+  // eslint-disable-next-line react/require-default-props
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
 const ButtonGroup = ({ children }) => {
   return <div className="button-group">{children}</div>;
 };
@@ -9,4 +19,31 @@ ButtonGroup.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-export default ButtonGroup;
+const Button = ({ className, children }) => {
+  const classNames = ['button', className].join(' ');
+  return <div className={classNames}>{children}</div>;
+};
+
+Button.defaultProps = buttonDefaulProps;
+
+Button.propTypes = buttonType;
+
+const SuccessButton = ({ className, children }) => {
+  const classNames = ['success', className].join(' ');
+  return <Button className={classNames}>{children}</Button>;
+};
+
+SuccessButton.defaultProps = buttonDefaulProps;
+
+SuccessButton.propTypes = buttonType;
+
+const SecondaryButton = ({ className, children }) => {
+  const classNames = ['secondary', className].join(' ');
+  return <Button className={classNames}>{children}</Button>;
+};
+
+SecondaryButton.defaultProps = buttonDefaulProps;
+
+SecondaryButton.propTypes = buttonType;
+
+export { Button, ButtonGroup, SuccessButton, SecondaryButton };
