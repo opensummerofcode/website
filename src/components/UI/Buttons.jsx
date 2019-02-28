bb99fc30dd8bf83f71df6ad29f17082426843a14
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-02-28 12:59:49 +0100

    Remove Button, SecondaryButton, ... Add ButtonLink

--- src/components/UI/Buttons.jsx
+++ src/components/UI/Buttons.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
-const buttonDefaulProps = {
-  className: ''
-};
-
-const buttonType = {
-  // eslint-disable-next-line react/require-default-props
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired
-};
+import ExternalLink from './ExternalLink';
 
 const ButtonGroup = ({ children }) => {
   return <div className="button-group">{children}</div>;
@@ -19,31 +11,14 @@ ButtonGroup.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-const Button = ({ className, children }) => {
+const ButtonLink = ({ className, children, isExternal = false, ...rest }) => {
   const classNames = ['button', className].join(' ');
-  return <div className={classNames}>{children}</div>;
+  const MyLink = isExternal ? ExternalLink : Link;
+  return (
+    <MyLink className={classNames} {...rest}>
+      {children}
+    </MyLink>
+  );
 };
 
-Button.defaultProps = buttonDefaulProps;
-
-Button.propTypes = buttonType;
-
-const SuccessButton = ({ className, children }) => {
-  const classNames = ['success', className].join(' ');
-  return <Button className={classNames}>{children}</Button>;
-};
-
-SuccessButton.defaultProps = buttonDefaulProps;
-
-SuccessButton.propTypes = buttonType;
-
-const SecondaryButton = ({ className, children }) => {
-  const classNames = ['secondary', className].join(' ');
-  return <Button className={classNames}>{children}</Button>;
-};
-
-SecondaryButton.defaultProps = buttonDefaulProps;
-
-SecondaryButton.propTypes = buttonType;
-
-export { Button, ButtonGroup, SuccessButton, SecondaryButton };
+export { ButtonLink, ButtonGroup };
