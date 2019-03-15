eaa228a3465efc50a0c9559f7826fa8e62e7f553
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-15 11:24:45 +0100

    Add new  props

--- src/components/Common/Tabs.jsx
+++ src/components/Common/Tabs.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tab from '../Common/Tab';
+import Tab from './Tab';
 
 // src: https://alligator.io/react/tabs-component/
 
 class Faqs extends React.Component {
   static propTypes = {
-    children: PropTypes.instanceOf(Array).isRequired
+    children: PropTypes.instanceOf(Array).isRequired,
+    contentClass: PropTypes.string.isRequired,
+    tabClass: PropTypes.string.isRequired,
+    tabListClass: PropTypes.string.isRequired,
+    activeClass: PropTypes.string.isRequired
   };
 
   constructor(props) {
@@ -24,28 +28,29 @@ class Faqs extends React.Component {
   render() {
     const {
       onClickTabItem,
-      props: { children },
+      props: { children, contentClass, tabClass, tabListClass, activeClass },
       state: { activeTab }
     } = this;
 
     return (
       <div className="tabs">
-        <ul className="tabs tabs--custom c-faq-tabs">
+        <ul className={tabListClass}>
           {children.map(child => {
             const { label } = child.props;
 
             return (
               <Tab
-                className="tabs-title"
+                className={tabClass}
                 activeTab={activeTab}
                 key={label}
                 label={label}
                 onClick={onClickTabItem}
+                activeClass={activeClass}
               />
             );
           })}
         </ul>
-        <div className="tabs-content c-faq-answers">
+        <div className={contentClass}>
           {children.map(child => {
             if (child.props.label !== activeTab) return undefined;
             return child.props.children;
