ca80192bf8d3266188b967ef4edf3157e965b373
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-15 11:13:10 +0100

    Implement Faqs component logic

--- src/components/Practical/faqs.jsx
+++ src/components/Practical/faqs.jsx
@@ -1,5 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Tab from '../Common/Tab';
 
-const Faqs = () => <h1>FAQ</h1>;
+// src: https://alligator.io/react/tabs-component/
+
+class Faqs extends React.Component {
+  static propTypes = {
+    children: PropTypes.instanceOf(Array).isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: this.props.children[0].props.label
+    };
+  }
+
+  onClickTabItem = tab => {
+    this.setState({ activeTab: tab });
+  };
+
+  render() {
+    const {
+      onClickTabItem,
+      props: { children },
+      state: { activeTab }
+    } = this;
+
+    return (
+      <div className="tabs">
+        <ul className="tabs tabs--custom c-faq-tabs">
+          {children.map(child => {
+            const { label } = child.props;
+
+            return (
+              <Tab
+                className="tabs-title"
+                activeTab={activeTab}
+                key={label}
+                label={label}
+                onClick={onClickTabItem}
+              />
+            );
+          })}
+        </ul>
+        <div className="tabs-content c-faq-answers">
+          {children.map(child => {
+            if (child.props.label !== activeTab) return undefined;
+            return child.props.children;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Faqs;
