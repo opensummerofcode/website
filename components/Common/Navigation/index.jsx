2a8bcb946f03d0d31a225c66293eab1c947639cd
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-03 11:27:16 +0200

    Make navigation stick via CSS

--- components/Common/Navigation/index.jsx
+++ components/Common/Navigation/index.jsx
@@ -4,77 +4,39 @@ import TextLogo from './Logo/TextLogo';
 import ImgLogo from './Logo/ImgLogo';
 import Tab from '../../UI/Tab';
 
-let Foundation;
 const Navigation = () => {
-  const STICKY = 'sticky is-at-top';
-  const $nav = useRef(null);
-
-  const [isSticky, setIsSticky] = useState(false);
-  const [height, setHeight] = useState(0);
-  const [canStick, setCanStick] = useState(null);
-  const [display, setDisplay] = useState('none');
-  const [className, setClassName] = useState('');
-
-  const setSticky = () => {
-    if (isSticky) setClassName(`${STICKY} is-stuck`);
-    else setClassName(`${STICKY} is-anchored`);
-  };
-
-  useEffect(setSticky, [isSticky]);
-
-  const calc = () => setHeight($nav.current.clientHeight);
-
-  useEffect(calc, [display]);
-
-  const handleToggle = () => {
-    if (canStick) return;
-    setDisplay(display ? '' : 'none');
-    calc();
-  };
+  const query = 'screen and (min-width: 64em)';
+  const [display, setDisplay] = useState('');
+  const [large, isLarge] = useState(false);
 
   const handleResize = () => {
-    const isLarge = Foundation.MediaQuery.atLeast('large');
-    if (display && isLarge) {
-      setSticky(true);
+    const matches = window.matchMedia(query).matches;
+    if (matches) {
+      isLarge(true);
       setDisplay('');
-      setCanStick(true);
-    } else if (!display && !isLarge) {
+    } else if (!matches) {
+      isLarge(false);
       setDisplay('none');
-      setCanStick(false);
-      setHeight(0);
-      setClassName('');
     }
   };
 
-  const handleScroll = () => {
-    if (!canStick) return;
-    const position = window.pageYOffset;
-    if (!isSticky && position > 0) {
-      setIsSticky(true);
-    } else if (isSticky && position === 0) setIsSticky(false);
-  };
-
   useEffect(() => {
-    import('foundation-sites').then(Mod => {
-      Foundation = Mod;
-      setCanStick(!!Foundation.MediaQuery.atLeast('large'));
-      setDisplay(Foundation.MediaQuery.atLeast('large') ? '' : 'none');
-      setClassName(Foundation.MediaQuery.atLeast('large') ? STICKY : '');
-    });
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    const matches = window.matchMedia(query).matches;
+    setDisplay(matches ? '' : 'none');
+    isLarge(window.matchMedia(query).matches);
     window.addEventListener('resize', handleResize);
-
     return () => {
-      window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', handleResize);
     };
-  });
+  }, []);
+
+  const handleToggle = () => {
+    if (large) return;
+    setDisplay(display ? '' : 'none');
+  };
 
   return (
-    <header>
+    <header style={{ position: large ? 'sticky' : 'relative', top: 0, zIndex: 5, bottom: 'auto' }}>
       <nav>
         <div
           className="title-bar hide-for-large"
@@ -93,24 +55,16 @@ const Navigation = () => {
           </div>
         </div>
 
-        <div className="sticky-container" data-sticky-container style={{ height }}>
+        <div>
           <div
-            className={`${className} top-bar bs--darken-light`}
+            className={`top-bar bs--darken-light`}
             id="nav-menu"
-            data-options="marginTop:0;"
-            ref={$nav}
-            style={{
-              width: '100%',
-              display,
-              marginTop: '0px',
-              bottom: 'auto',
-              top: 0
-            }}
+            style={{ display: large ? '' : display }}
           >
             <div className="top-bar-left show-for-large">
               <Link href="/">
                 <a className="logo" data-hide-for="medium">
-                  <ImgLogo handleLoad={calc} />
+                  <ImgLogo />
                 </a>
               </Link>
             </div>
