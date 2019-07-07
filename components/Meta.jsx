ab90159b1fbd5a070b006126696267d81b69dd3b
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-07 22:22:00 +0200

    Move and implement favicons

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 const Meta = () => (
   <Head>
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="theme-color" content="#000000" />
     <link
@@ -14,8 +13,53 @@ const Meta = () => (
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"
     />
-    <link rel="manifest" href="/manifest.json" />
+    <link rel="manifest" href="static/manifest.json" />
     <title>open Summer of code 2019</title>
+    {/* Favicons */}
+    <link rel="shortcut icon" href="/static/favicon.ico" />
+    <link
+      rel="apple-touch-icon"
+      sizes="57x57"
+      href="/static/static/favicons/apple-touch-icon-57x57.png"
+    />
+    <link rel="apple-touch-icon" sizes="60x60" href="/static/favicons/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/static/favicons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon-76x76.png" />
+    <link
+      rel="apple-touch-icon"
+      sizes="114x114"
+      href="/static/favicons/apple-touch-icon-114x114.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="/static/favicons/apple-touch-icon-120x120.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="144x144"
+      href="/static/favicons/apple-touch-icon-144x144.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="/static/favicons/apple-touch-icon-152x152.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/static/favicons/apple-touch-icon-180x180.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="/static/favicons/android-chrome-192x192.png"
+      sizes="192x192"
+    />
+    <link rel="manifest" href="/static/favicons/manifest.json" />
+    <meta name="msapplication-TileColor" content="‪#FFFFFF" />
+    <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+    <meta name="theme-color" content="‪#FFFFFF" />
   </Head>
 );
 
