345fd26d45e0f43578e1f07a0654021f2d7779ad
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-08 22:53:14 +0200

    Update Student FAQ

--- components/Practical/StudentApplications.jsx
+++ components/Practical/StudentApplications.jsx
@@ -2,159 +2,174 @@ import ExternalLink from '../UI/ExternalLink';
 
 const StudentApplication = () => {
   return (
-    <div>
-      <div className="grid-x grid-padding-x u-margin-large--tb">
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">Why should I participate in osoc? </h4>
-            <div className="c-faq__item__answer">
-              <p>
-                There are multiple reasons to participate. First of all, you can improve your skills
-                under the supervision of several experienced coaches. Secondly, since you’ll be
-                making a real product or service, it looks great on your resume and you’ll expand
-                your network. Thirdly, you’ll surely make some new friends too.
-              </p>
-              <p>
-                Sounds awesome, no? And we didn’t even get to the best part yet: osoc is completely
-                free. Even better, you are getting paid! This programme could replace your summer
-                job. Enhancing your skills, making new friends and cashing in some money too? Sounds
-                like a good plan, right?
-              </p>
-            </div>
+    <div className="grid-x grid-padding-x u-margin-large--tb">
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">Why should I participate in osoc? </h4>
+          <div className="c-faq__item__answer">
+            <p>
+              There are multiple reasons to participate. First of all, you can improve your skills
+              under the supervision of several experienced coaches. Secondly, since you’ll be making
+              a real product or service, it looks great on your resume and you’ll expand your
+              network. Thirdly, you’ll surely make some new friends too.
+            </p>
+            <p>
+              Sounds awesome, no? And we didn’t even get to the best part yet: osoc is completely
+              free. Even better, you are getting paid! This programme could replace your summer job.
+              Enhancing your skills, making new friends and cashing in some money too? Sounds like a
+              good plan, right?
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">What will I be doing during osoc?</h4>
-            <div className="c-faq__item__answer">
-              <p>
-                All students will work in teams on different open innovation projects provided by
-                partnering organisations, companies and governments. The goal is to complete these
-                projects by the end of the programme and turn them into real-world products and
-                services. In the end, your team will present the finished product to the partnering
-                organisation and the world.
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">What will I be doing during osoc?</h4>
+          <div className="c-faq__item__answer">
+            <p>
+              All students will work in teams on different open innovation projects provided by
+              partnering organisations, companies and governments. The goal is to complete these
+              projects by the end of the programme and turn them into real-world products and
+              services. In the end, your team will present the finished product to the partnering
+              organisation and the world.
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">
-              What kind of project can I expect to work on at osoc?
-            </h4>
-            <div className="c-faq__item__answer">
-              <p>
-                All projects at osoc are being built open source and aim to create real-world
-                impact. Over the last few years, students have got the opportunity to work on open
-                innovation projects in different fields such as mobility, education, health, sports
-                and biodiversity. Check out <a href="2018.html">this year&apos;s edition</a> to see
-                what kind of projects have been built by osoc students at the latest edition.
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">
+            What kind of project can I expect to work on at osoc?
+          </h4>
+          <div className="c-faq__item__answer">
+            <p>
+              All projects at osoc are being built open source and aim to create real-world impact.
+              Over the last few years, students have got the opportunity to work on open innovation
+              projects in different fields such as mobility, education, health, sports and
+              biodiversity. Check out{' '}
+              <ExternalLink href="http://2018.summerofcode.be" target="_blank">
+                last year&quot;s edition
+              </ExternalLink>{' '}
+              to see what kind of projects have been built by osoc students at the latest edition.
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">Can I participate in osoc?</h4>
-            <div className="c-faq__item__answer">
-              <p>Yes, if you meet the following requirements.</p>
-              <ul>
-                <li>
-                  First of all, you’re a student eligible to work under a student contract for 16
-                  full days (128 hours).
-                </li>
-                <li>
-                  Secondly, you study or have experience in front- or back-end development, design
-                  (UX, graphic), communication or business modelling.
-                </li>
-                <li>Thirdly, you should be able to express yourself in English.</li>
-              </ul>
-              <p>Do you meet these requirements? Congratulations, you could be an osoc student!</p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">Can I participate in osoc?</h4>
+          <div className="c-faq__item__answer">
+            <p>Yes, if you meet the following requirements.</p>
+            <ul>
+              <li>
+                First of all, you’re a student eligible to work under a student contract for 16 full
+                days (128 hours).
+              </li>
+              <li>
+                Secondly, you study or have experience in front- or back-end development, design
+                (UX, graphic), communication or business modelling.
+              </li>
+              <li>Thirdly, you should be able to express yourself in English.</li>
+            </ul>
+            <p>Do you meet these requirements? Congratulations, you could be an osoc student!</p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">
-              How do I apply for osoc? When will I know whether I can participate?{' '}
-            </h4>
-            <div className="c-faq__item__answer">
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">
+            How do I apply for osoc? When will I know whether I can participate?{' '}
+          </h4>
+          <div className="c-faq__item__answer">
+            <p>
               <p>
                 Sorry, applications are closed as of now. Thank you for your massive interest! You
                 can always apply again next year as a student &mdash; or even as a coach.
-                You&apos;re also welcome to join us during the{' '}
-                <ExternalLink href="https://t.co/EhoiMCJRnT">demo day</ExternalLink> &mdash; which
-                will take place Thursday, 26 June this year. And keep an eye on our{' '}
-                <ExternalLink href="https://twitter.com/osocode">Twitter account</ExternalLink> to
-                see what we&apos;re up to!
+                You&quot;re also welcome to join us during the{' '}
+                <ExternalLink href="https://demodayosoc19.eventbrite.co.uk/" target="_blank">
+                  demo day
+                </ExternalLink>{' '}
+                &mdash; which will take place Thursday, 25 July this year. And keep an eye on our{' '}
+                <ExternalLink href="https://twitter.com/osocode" target="_blank">
+                  Twitter account
+                </ExternalLink>{' '}
+                to see what we&quot;re up to!
               </p>
-              {/* <!-- <p>In May we go over all the applications and match the right students with the right projects. After that, we’ll let you know whether you’re in or not.</p> --> */}
-              {/* <!-- <p><a href="https://goo.gl/forms/KkRSdUxs19hVv2H62" title="Click here to apply as a student">Apply here</a>.</p> --> */}
-            </div>
+              {/* <p><a href="https://osoc.typeform.com/to/HPSHZt" title="Click here to apply as a student">Apply here</a>.</p>
+              <p>In May we go over all the applications and match the right students with the right projects. After that, we’ll let you know whether you've been selected.</p> */}
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">How are we going to be assessed?</h4>
-            <div className="c-faq__item__answer">
-              <p>
-                In order to apply you need to submit a letter of motivation, explaining why you
-                would like to join the movement and what you will bring to the table to make osoc
-                great. Make sure to include your portfolio or some examples of your work as well.
-                Based on your motivation, portfolio and your CV, a group of experienced osoc
-                volunteers will match your unique set of skills and experiences with partner’s
-                desired student profiles.
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">How are you going to be assessed?</h4>
+          <div className="c-faq__item__answer">
+            <p>
+              In order to apply you need to submit a letter of motivation, explaining why you would
+              like to join the movement and what you will bring to the table to make osoc great.
+              Make sure to include your portfolio or some examples of your work as well. Based on
+              your motivation, portfolio and your CV, a group of experienced osoc volunteers will
+              match your unique set of skills and experiences with partner’s desired student
+              profiles.
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">What if I can&apos;t join osoc?</h4>
-            <div className="c-faq__item__answer">
-              <p>
-                That&apos;s too bad! You can always apply again next year as a student &mdash; or
-                even as a coach. You&apos;re also welcome to join us during the{' '}
-                <ExternalLink href="https://t.co/EhoiMCJRnT">demo day</ExternalLink> &mdash; which
-                will take place Thursday, 26 June this year. And keep an eye on our{' '}
-                <ExternalLink href="https://twitter.com/osocode">Twitter account</ExternalLink> to
-                see what we&apos;re up to!
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">What if I can&quot;t join osoc?</h4>
+          <div className="c-faq__item__answer">
+            <p>
+              That&quot;s too bad! You can always apply again next year as a student &mdash; or even
+              as a coach. You&quot;re also welcome to join us during the{' '}
+              <ExternalLink href="https://demodayosoc19.eventbrite.co.uk/" target="_blank">
+                demo day
+              </ExternalLink>{' '}
+              &mdash; which will take place Thursday, 25 July this year. And keep an eye on our{' '}
+              <ExternalLink href="https://twitter.com/osocode" target="_blank">
+                Twitter account
+              </ExternalLink>{' '}
+              to see what we&quot;re up to!
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">
-              What if I didn&apos;t make it through the applications?
-            </h4>
-            <div className="c-faq__item__answer">
-              <p>
-                Don&apos;t worry, you can always apply again next year as a student &mdash; or even
-                as a coach. Just keep working on yourself and your resumé. We will review your
-                application like it&apos;s the first time you applied. Don&apos;t forget to visit us
-                during the <ExternalLink href="https://t.co/EhoiMCJRnT">demo day</ExternalLink> ;-).
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">
+            What if I didn&quot;t make it through the applications?
+          </h4>
+          <div className="c-faq__item__answer">
+            <p>
+              Don&quot;t worry, you can always apply again next year as a student &mdash; or even as
+              a coach. Just keep working on yourself and your resumé. We will review your
+              application like it&quot;s the first time you applied. Don&quot;t forget to visit us
+              during the{' '}
+              <ExternalLink href="https://demodayosoc19.eventbrite.co.uk/" target="_blank">
+                demo day
+              </ExternalLink>{' '}
+              ;-).
+            </p>
           </div>
         </div>
+      </div>
 
-        <div className="cell small-11 medium-8 large-6">
-          <div className="c-faq__item">
-            <h4 className="c-faq__item__question">Reimbursements</h4>
-            <div className="c-faq__item__answer">
-              <p>
-                We compensate all your public transports costs. However, you are free to come by car
-                or book yourself a fancy hotel, but we don&apos;t reimburse those things.
-              </p>
-            </div>
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">Reimbursements</h4>
+          <div className="c-faq__item__answer">
+            <p>
+              We compensate all your public transports costs. You are free to come by car or book
+              yourself a fancy hotel, but we don&quot;t reimburse those things.
+            </p>
           </div>
         </div>
       </div>
