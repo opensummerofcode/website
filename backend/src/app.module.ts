7e68ac795c7f3f602bda2ca6dc970f9eb50bad64
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-18 11:55:19 +0200

    Add graphql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
