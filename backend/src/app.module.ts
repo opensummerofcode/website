12d20941f4b01f6579312e8cc27e5299f837303d
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-18 14:02:57 +0200

    Add mongoose & dotenv

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({}),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('MONGODB_URI'),
+        dbName: config.get('DB_NAME'),
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
