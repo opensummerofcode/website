a31ebd3edd531738b6abc4314e8fc1569f0226e9
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-19 19:10:57 +0200

    Feature some projects

--- components/Projects/FeaturedProjects.jsx
+++ components/Projects/FeaturedProjects.jsx
@@ -1,70 +1,38 @@
-import ExternalLink from '../UI/ExternalLink';
-import YearEditionCall from '../Common/CallToActions';
+import Link from 'next/link';
+import { ButtonLink } from '../UI/Buttons';
+
+import projects from '../../assets/data/projects.json';
 
 const FeaturedProjects = ({ divider }) => {
+  const renderProject = ({ name, description, logo, id }) => (
+    <div key={id} className="small-6 medium-6 large-3 cell c-projects-project">
+      <Link href={`/2019/${id}`}>
+        <a className="c-projects-image">
+          <img src={logo} alt="Logo of project Bike4Brussels" />
+        </a>
+      </Link>
+      <div className="c-projects-content">
+        <h2 className="h5">
+          <Link href={`/2019/${id}`}>
+            <a>{name}</a>
+          </Link>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+
+  const $projects = projects.filter(p => p.featured).map(renderProject);
   return (
     <div className={`u-padding-x-large--tb c-projects-old ${divider}`}>
       <div className="grid-x grid-padding-x align-center">
-        <div className="small-6 medium-6 large-3 cell c-projects-project">
-          <ExternalLink href="https://github.com/oSoc18/bike4brussels" className="c-projects-image">
-            <img src="/static/img/projects/bike4brussels.png" alt="Logo of project Bike4Brussels" />
-          </ExternalLink>
-          <div className="c-projects-content">
-            <h2 className="h5">
-              <ExternalLink href="https://github.com/oSoc18/bike4brussels">
-                Bike4Brussels
-              </ExternalLink>
-            </h2>
-            <p>Get the best route for your bike ride in Brussels!</p>
-          </div>
-        </div>
-
-        <div className="small-6 medium-6 large-3 cell c-projects-project">
-          <ExternalLink href="https://github.com/oSoc18/bitsoftrust" className="c-projects-image">
-            <img src="/static/img/projects/bitsoftrust.png" alt="Logo of project Bits of Trust" />
-          </ExternalLink>
-          <div className="c-projects-content">
-            <h2 className="h5">
-              <ExternalLink href="https://github.com/oSoc18/bitsoftrust">
-                Bits of Trust
-              </ExternalLink>
-            </h2>
-            <p>
-              Get acknowledged for your skills by letting your peers endorse and verify your
-              qualities in an easy, reliable and standardized way.
-            </p>
-          </div>
-        </div>
-
-        <div className="small-6 medium-6 large-3 cell c-projects-project">
-          <ExternalLink href="https://github.com/oSoc18/fris" className="c-projects-image">
-            <img src="/static/img/projects/fris.png" alt="Logo of project FRIS" />
-          </ExternalLink>
-          <div className="c-projects-content">
-            <h2 className="h5">
-              <ExternalLink href="https://github.com/oSoc18/fris">FRIS</ExternalLink>
-            </h2>
-            <p>An expert-finding tool to bring researchers and companies together.</p>
-          </div>
-        </div>
-
-        <div className="small-6 medium-6 large-3 cell c-projects-project">
-          <ExternalLink href="https://github.com/oSoc18/gentlestudent" className="c-projects-image">
-            <img src="/static/img/projects/gentlestudent.png" alt="Logo of project Gentlestudent" />
-          </ExternalLink>
-          <div className="c-projects-content">
-            <h2 className="h5">
-              <ExternalLink href="https://github.com/oSoc18/gentlestudent">
-                Gentlestudent
-              </ExternalLink>
-            </h2>
-            <p>
-              Motivates students to engage in learning opportunities using the Open Badges Standard.
-            </p>
-          </div>
-        </div>
-
-        <YearEditionCall className="medium-7 large-4 c-projects-old__project align-center text-center" />
+        {$projects}
+        <ButtonLink
+          className="medium-7 large-4 c-projects-old__project align-center text-center u-margin-large--t"
+          href="/2019"
+        >
+          View this year&apos;s projects
+        </ButtonLink>
       </div>
     </div>
   );
