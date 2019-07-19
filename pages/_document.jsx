0d567ae00d79c736f921717a5cd6e667279cf019
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-19 17:38:54 +0200

    Add font-display 'swap'

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link
-            href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Open+Sans:300,400"
+            href="https://fonts.googleapis.com/css?font-display=swap&amp;family=Montserrat:400,500,600,700|Open+Sans:300,400"
             rel="stylesheet"
           />
           <link
