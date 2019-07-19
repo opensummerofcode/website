c6bbe90b16ae7bf12daf3a555d8c54d8b35e927f
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-19 19:13:06 +0200

    Increase page transition time

--- components/UI/PageTransition.jsx
+++ components/UI/PageTransition.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
 
-const timeout = 200;
+const timeout = 300;
 const getTransitionStyles = {
   entering: {
     position: 'absolute',
