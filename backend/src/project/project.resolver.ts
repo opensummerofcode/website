d8aa6288901d02037c299d1bf2e812f07c46a5af
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-20 21:24:36 +0200

    Allow adding of linked projects, team, partners & implement related field resolvers

--- backend/src/project/project.resolver.ts
+++ backend/src/project/project.resolver.ts
@@ -1,10 +1,17 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveProperty,
+  Parent,
+} from '@nestjs/graphql';
 import { Project } from './types/project.type';
 import { ProjectService } from './project.service';
 import { NewProjectInput } from './inputs/new.project.input';
 import { UpdateProjectInput } from './inputs/update.project.input';
 
-@Resolver('Project')
+@Resolver(of => Project)
 export class ProjectResolver {
   constructor(private readonly projectService: ProjectService) {}
 
@@ -24,5 +31,44 @@ export class ProjectResolver {
     @Args('input') input: UpdateProjectInput,
   ): Promise<Project> {
     return this.projectService.update({ projectId, update: input });
+  }
+
+  @Mutation(returns => Project)
+  addParticipantToProject(
+    @Args('projectId') projectId: string,
+    @Args('participantId') participantId: string,
+  ): Promise<Project> {
+    return this.projectService.addParticipant(projectId, participantId);
+  }
+
+  @Mutation(returns => Project)
+  addPartnerToProject(
+    @Args('projectId') projectId: string,
+    @Args('partnerId') partnerId: string,
+  ): Promise<Project> {
+    return this.projectService.addPartner(projectId, partnerId);
+  }
+
+  @Mutation(returns => Project)
+  bind(
+    @Args('projectId1') projectId1: string,
+    @Args('projectId2') projectId2: string,
+  ): Promise<Project> {
+    return this.projectService.bind(projectId1, projectId2);
+  }
+
+  @ResolveProperty()
+  linkedTo(@Parent() project) {
+    return this.projectService.bonds(project);
+  }
+
+  @ResolveProperty()
+  team(@Parent() project) {
+    return this.projectService.team(project);
+  }
+
+  @ResolveProperty()
+  partners(@Parent() project) {
+    return this.projectService.partners(project);
   }
 }
--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -21,9 +21,22 @@ export class ProjectService {
     return this.projectModel.create(input);
   }
 
-  async saveLogo(logo): Promise<string> {
+  private async saveLogo(logo): Promise<string> {
     const file = logo && (await this.fileService.store(await logo, 'projects'));
     return file && file.secure_url;
+  }
+
+  private async populate(project: IProject, field: string) {
+    const populated = await project.populate(field).execPopulate();
+    return populated[field];
+  }
+
+  private async push(
+    projectId: string,
+    element: string,
+    field: string,
+  ): Promise<IProject> {
+    return this.update({ projectId, update: { $push: { [field]: element } } });
   }
 
   async update({ projectId, update }): Promise<IProject> {
@@ -35,5 +48,31 @@ export class ProjectService {
         new: true,
       },
     );
+  }
+
+  async bind(projectId1: string, projectId2: string): Promise<IProject> {
+    const field = 'linkedTo';
+    await this.push(projectId2, projectId1, field);
+    return this.push(projectId1, projectId2, field);
+  }
+
+  bonds(project: IProject) {
+    return this.populate(project, 'linkedTo');
+  }
+
+  addPartner(projectId: string, partnerId: string): Promise<IProject> {
+    return this.push(projectId, partnerId, 'partners');
+  }
+
+  partners(project: IProject): Promise<IProject> {
+    return this.populate(project, 'partners');
+  }
+
+  addParticipant(projectId: string, participantId: string): Promise<IProject> {
+    return this.push(projectId, participantId, 'team');
+  }
+
+  team(project: IProject) {
+    return this.populate(project, 'team');
   }
 }
