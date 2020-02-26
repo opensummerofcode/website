25ce72476d23be2ab9088948214dd8a9a5a58269
Michiel Leyman <michiel.leyman@gmail.com>
2020-02-26 15:30:30 +0100

    Remove unused tests and data fixtures

--- components/Companies/Partners/Partners.test.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import Partners from '.';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Partners />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
