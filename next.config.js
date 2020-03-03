7a97844bc60fead23ded6559ee220a7c4a119f5c
Michiel Leyman <michiel.leyman@gmail.com>
2020-03-03 13:42:45 +0100

    Temporarily restore static export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 
-module.exports = withSass(withImages({}));
+const projects = require('./public/editions/2019/projects.json');
+
+module.exports = withSass(
+  withImages({
+    exportPathMap() {
+      const projectPages = projects.reduce((pages, project) => {
+        return {
+          ...pages,
+          [`/editions/2019/${project.id}`]: {
+            page: `/editions/2019/[project]`,
+            query: { project: project.id }
+          }
+        };
+      }, {});
+      return {
+        '/': { page: '/' },
+        '/editions/2019': { page: '/editions/2019' },
+        '/coaches': { page: '/coaches' },
+        '/companies': { page: '/companies' },
+        '/practical': { page: '/practical' },
+        '/students': { page: '/students' },
+        ...projectPages
+      };
+    }
+  })
+);
