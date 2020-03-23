9896ab5375ca62eca3c5f4d302bbad940e37af4f
Michiel Leyman <michiel.leyman@gmail.com>
2020-03-23 17:29:20 +0100

    Add COVID-19 notice

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import useSWR from 'swr';
 import MetaContext from '../context/meta';
 import fetch from '../util/fetch';
@@ -6,9 +6,11 @@ import OsocDescription from '../components/Home/OsocDescription';
 import Braggings from '../components/Home/Braggings';
 import HomeHeader from '../components/Home/HomeHeader';
 import Partners from '../components/Companies/Partners';
+import ExternalLink from '../components/UI/ExternalLink';
 
 const HomeContainer = () => {
   const { previousEdition, showPreviousPartners } = useContext(MetaContext);
+  const [infoNoticeShown, setInfoNoticeShown] = useState(true);
 
   const partners = showPreviousPartners
     ? useSWR(() => `/editions/${previousEdition.year}/partners.json`, fetch).data
@@ -20,6 +22,27 @@ const HomeContainer = () => {
       <OsocDescription />
       <Braggings />
       <Partners partners={partners} />
+      {infoNoticeShown && (
+        <div className="covid-notice">
+          {/* eslint-disable-next-line */}
+          <i
+            className="covid-notice__close fa fa-close"
+            role="button"
+            onClick={() => setInfoNoticeShown(false)}
+          />
+          <div className="covid-notice__info-icon">
+            <i className="fa fa-info" />
+          </div>
+          <div>
+            This year&apos;s edition of Open Summer of Code is <strong>guaranteed</strong>, despite{' '}
+            <ExternalLink href="https://en.wikipedia.org/wiki/Coronavirus_disease_2019">
+              the COVID-19 pandemic
+            </ExternalLink>
+            . If social distancing measures are still required in July,{' '}
+            <ExternalLink href="https://twitter.com/osocode">#osoc20</ExternalLink> will be remote.
+          </div>
+        </div>
+      )}
     </>
   );
 };
