92d0b065a7778c4acceff7a36394b49d53607d94
Jonathan Beliën <dev@jbelien.be>
2021-01-30 12:03:59 +0100

    Upgrade dependencies (backward compatible)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withImages = require('next-images');
 
 module.exports = withSass(
   withImages({
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      });
+      return config;
+    },
     env: {
       ENV: process.env.NODE_ENV,
       HOST_URL: process.env.HOST_URL || `http://localhost:${process.env.PORT}`
