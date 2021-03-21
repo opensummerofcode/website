baa33426300dc6f9dea36df1da46b9ee625ee2b8
Jonathan Beliën <dev@jbelien.be>
2021-03-21 11:34:00 +0100

    Update student application link (2021)

--- components/Common/Navigation/index.jsx
+++ components/Common/Navigation/index.jsx
@@ -97,18 +97,17 @@ const Navigation = () => {
                 </li>
                 */}
 
-                {/*
-                  <li>
+                <li>
                   <ExternalLink
-                    href="https://osoc.typeform.com/to/UhvzI4"
+                    href="https://tally.so/r/npe0Bm"
                     target="_blank"
                     className="button"
                   >
                     Apply as a student
-                    <span className="button__info">before 22nd of May</span>
+                    {/* <span className="button__info">before 22nd of May</span> */}
                   </ExternalLink>
                 </li>
-                  */}
+
                 {/*
                         <li>
                   {' '}
