985cadb941de8e3bb780cf93a5f39c3187e7c754
Jonathan Beliën <dev@jbelien.be>
2021-03-23 18:54:06 +0100

    Replace @zeit/next-sass by sass dependency

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 
-module.exports = withSass(
-  withImages({
-    env: {
-      ENV: process.env.NODE_ENV,
-      HOST_URL: process.env.HOST_URL || `http://localhost:${process.env.PORT}`
-    }
-  })
-);
+module.exports = withImages({
+  env: {
+    ENV: process.env.NODE_ENV,
+    HOST_URL: process.env.HOST_URL || `http://localhost:${process.env.PORT}`
+  }
+});
