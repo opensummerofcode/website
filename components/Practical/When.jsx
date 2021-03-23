0d7f01b818ade839c49933c5bd794e7e2ef58283
Jonathan Beliën <dev@jbelien.be>
2021-03-23 19:19:55 +0100

    Update When.jsx

--- components/Practical/When.jsx
+++ components/Practical/When.jsx
@@ -9,10 +9,10 @@ const When = () => {
           </h3>
           <p>You don&apos;t have to work on Fridays, holidays or weekends!</p>
 
-         {/* <p>
+          <p>
             Note: this is last year&apos;s schedule for reference. This year&apos;s planning will be
             published in May.
-          </p>*/}
+          </p>
           <div className="loading loading--calendar">
             {/* <!-- <iframe class="calendar" src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=mietclaes.com_h1rs31a79809r742e5lpdpegbg%40group.calendar.google.com&amp;color=%2342104A&amp;ctz=Europe%2FBrussels&dates=20180702%2F20180726" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe> --> */}
             <iframe
