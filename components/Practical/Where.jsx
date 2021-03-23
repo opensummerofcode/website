9ccc8968d3219f3ea155af1fb915eaaf2b3f8f3a
Jonathan Beliën <jbelien@users.noreply.github.com>
2021-03-23 21:24:02 +0100

    Use Leaflet instead of OSM iframe + Create locations.json (#71)
    * Use Leaflet instead of OSM iframe + Create locations.json
    
    Close #41
    
    * Update _header.scss
    
    Increase header z-index to be on top of the Leafleat map.
    
    * Update Leaflet.jsx
    
    Increase CircleMarker radius.
    
    * Update Where.jsx
    
    Hide locations because online edition.

--- components/Practical/Where.jsx
+++ components/Practical/Where.jsx
@@ -1,117 +1,39 @@
-const Where = () => {
-  return (
-    <>
-      <div className="grid-x grid-padding-x u-margin-large--tb">
-        <div className="small-12 medium-6 large-5 cell">
-          <h3 className="h5">
-            TBD
-            <br />
-            <span className="subheader">Brussels, 6 July &mdash; 16 July</span>
-          </h3>
-          {/*
-            <p className="u-margin--t">
-              <strong>WTC III building</strong>
-              <br />
-              Boulevard Simon Bolivar 30, 1000 Brussels
-            </p>
-          */}
-        </div>
-        <div className="small-12 medium-6 large-7 cell">
-          <div className="loading loading--map u-margin--b">
-            {/*
-            <iframe
-              title="Location of BOSA building"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://www.openstreetmap.org/export/embed.html?bbox=4.353718757629395%2C50.859723986123385%2C4.357932507991792%2C50.86184196498779&amp;layer=mapnik&amp;marker=50.86078298758114%2C4.356158226728439"
-              style={{ width: '100%', height: '250px' }}
-            />
-            <br />
-            <small>
-              <a href="https://www.openstreetmap.org/#map=19/50.86078/4.35583">View Larger Map</a>
-            </small>
-            */}
-            <iframe
-              title="unknown location"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://www.openstreetmap.org/export/embed.html?bbox=4.352624416351319%2C50.842722493832355%2C4.3622803688049325%2C50.84820293447596&amp;layer=mapnik&amp;"
-              style={{ width: '100%', height: '250px' }}
-            />
-            <div className="text-right">
-              <small>
-                <a href="https://www.openstreetmap.org/?mlat=50.84546&amp;mlon=4.35745#map=17/50.84546/4.35745">
-                  View Larger Map
-                </a>
-              </small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div className="grid-x grid-padding-x u-margin-large--tb">
-        <div className="small-12 medium-6 large-5 cell">
-          <h3 className="h5">
-            {/* Tour &amp; Taxis */}
-            TBD
-            <br />
-            <span className="subheader">Brussels, 20 July &mdash; 30 July</span>
-          </h3>
-          {/*
-          <p className="u-margin--t">
-            <strong>Flemish Government&apos;s Herman Teirlinck building</strong>
-            <br />
-            Avenue du Port 88, 1000 Bruxelles
-          </p>
-          */}
-        </div>
-        <div className="small-12 medium-6 large-7 cell">
-          <div className="loading loading--map u-margin--b">
-            {/*
-            <iframe
-              title="Location of Herman Teirlinck Building"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://www.openstreetmap.org/export/embed.html?bbox=4.345307350158692%2C50.86345535675067%2C4.354963302612306%2C50.868933361672724&amp;layer=mapnik&amp;marker=50.8661944396736%2C4.350135326385498"
-              style={{ width: '100%', height: '250px' }}
-            />
-            */}
-            <iframe
-              title="unknown location"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://www.openstreetmap.org/export/embed.html?bbox=4.352624416351319%2C50.842722493832355%2C4.3622803688049325%2C50.84820293447596&amp;layer=mapnik&amp;"
-              style={{ width: '100%', height: '250px' }}
-            />
-            <div className="text-right">
-              <small>
-                <a href="https://www.openstreetmap.org/?mlat=50.86619&amp;mlon=4.35014#map=17/50.86619/4.35014">
-                  View Larger Map
-                </a>
-              </small>
-            </div>
-          </div>
-        </div>
-      </div>
+import useSWR from 'swr';
+import fetch from '../../util/fetch';
+import Location from './Where/Location';
 
-      <hr />
-      <div className="grid-x grid-padding-x u-margin-large--tb">
-        <div className="small-10 medium-6 large-5 cell">
-          <p>
-            We compensate all your public transports costs. However, you are free to come by car or
-            book yourself a fancy hotel, but we don&apos;t reimburse those things.
-          </p>
+const Where = () => {
+  const renderLocation = location => (
+    <Location name={location.name} address={location.address} coordinates={location.coordinates} />
+  );
+
+  const { data: locations } = useSWR(`/locations.json`, fetch);
+  if (!locations) return <></>;
+  const $locations = locations.map(l => renderLocation(l));
+
+  return (
+    <div className="grid-x grid-padding-x u-margin-large--tb">
+      <div className="cell small-11 medium-8 large-6">
+        <div className="c-faq__item">
+          <h4 className="c-faq__item__question">Online edition</h4>
+          <div className="c-faq__item__answer">
+            <p>Due to the current situation, this year edition will be an online edition.</p>
+          </div>
         </div>
       </div>
-    </>
+    </div>
+    // <>
+    //   {$locations}
+
+    //   <div className="grid-x grid-padding-x u-margin-large--tb">
+    //     <div className="small-10 medium-6 large-5 cell">
+    //       <p>
+    //         We compensate all your public transports costs. However, you are free to come by car or
+    //         book yourself a fancy hotel, but we don&apos;t reimburse those things.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </>
   );
 };
 
---
+++ components/Practical/Where/Leaflet.jsx
@@ -0,0 +1,18 @@
+import { Map as LeafletMap, TileLayer, CircleMarker } from 'react-leaflet-universal';
+
+const Leaflet = ({ longitude, latitude }) => {
+  const position = [latitude, longitude];
+
+  return (
+    <LeafletMap center={position} zoom={15} style={{ height: '250px' }}>
+      <TileLayer
+        url="https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png"
+        attribution='&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a target="_blank" href="https://geo6.be/">GEO-6</a>'
+      />
+      {/* @todo Replace CircleMarker by Marker ; issue with the url of the marker icon. */}
+      <CircleMarker center={position} radius={10} />
+    </LeafletMap>
+  );
+};
+
+export default Leaflet;
