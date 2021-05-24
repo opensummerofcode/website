b44961344981d6dd75b6a013f207aeb4fd52c59f
Jonathan Beliën <jbelien@users.noreply.github.com>
2021-05-24 16:06:01 +0200

    Update index.jsx
    Hide application form link from header.

--- components/Common/Navigation/index.jsx
+++ components/Common/Navigation/index.jsx
@@ -97,12 +97,14 @@ const Navigation = () => {
                 </li>
                 */}
 
+                {/*
                 <li>
                   <ExternalLink href="https://tally.so/r/npe0Bm" target="_blank" className="button">
                     Apply as a student
                     <span className="button__info">before 23 May</span>
                   </ExternalLink>
                 </li>
+                */}
 
                 {/*
                         <li>
