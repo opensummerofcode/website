62fdab6da36f2a16ae92a2419b37548e9fdcd962
Jonathan Beliën <dev@jbelien.be>
2021-07-20 10:14:09 +0200

    Move DemoDayLink.jsx to UI folder

--- components/UI/DemoDayLink.jsx
+++ components/UI/DemoDayLink.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import ExternalLink from '../UI/ExternalLink';
+import ExternalLink from './ExternalLink';
 
 const DemoDayLink = ({ className, children: text }) => {
   return (
