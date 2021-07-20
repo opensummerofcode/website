e9b662cde4062e55262159a56df7649fe73ba9ca
Jonathan Beliën <dev@jbelien.be>
2021-07-20 09:58:26 +0200

    Update DemoDayLink.jsx

--- components/Practical/DemoDayLink.jsx
+++ components/Practical/DemoDayLink.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 import ExternalLink from '../UI/ExternalLink';
 
-const demoDayLink = null;
-
-const DemoDayLink = ({ children: text }) => {
-  if (!demoDayLink) return <>{text}</>;
+const DemoDayLink = ({ className, children: text }) => {
   return (
-    <ExternalLink href="https://demodayosoc20.eventbrite.co.uk/" target="_blank">
+    <ExternalLink href="https://demodayosoc21.eventbrite.be/" className={className} target="_blank">
       {text}
     </ExternalLink>
   );
 };
 
+DemoDayLink.defaultProps = {
+  className: '',
+};
+
 DemoDayLink.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.element.isRequired,
 };
 
