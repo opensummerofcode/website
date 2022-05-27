c4c5929ad5a7ecdf4f9a9ec0dc8c43498354f5df
Miet <github@miet.be>
2022-05-27 11:22:38 +0200

    Update calendar to new dates
    it is now in 2022, hybrid

--- components/Practical/When.jsx
+++ components/Practical/When.jsx
@@ -4,11 +4,10 @@ const When = () => {
       <div className="grid-x grid-padding-x u-margin-large--tb">
         <div className="small-12 cell">
           <h3 className="h5">
-            {/* TODO: replace by dynamic date */}5 July &mdash; 29 July{' '}
-            <span className="subheader">From Monday to Thursday, 9AM to 5PM</span>
+            {/* TODO: replace by dynamic date */}4 July &mdash; 28 July{' '}
+            <span className="subheader">From Monday to Thursday, 9AM to 5PM.</span>
           </h3>
-          <p>You don&apos;t have to work on Fridays, holidays or weekends!</p>
-          <p>Example calendar, the real one is coming up.</p>
+          <p>On Mondays and Thursdays we work in <strong>Brussels</strong>, on Tuesdays and Wednesdays <strong>hybrid</strong>. You don&apos;t have to work on Fridays, holidays or weekends!</p>
 
           {/* <p>
             Note: this is last year&apos;s schedule for reference. This year&apos;s planning will be
@@ -18,7 +17,7 @@ const When = () => {
             {/* <!-- <iframe class="calendar" src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=mietclaes.com_h1rs31a79809r742e5lpdpegbg%40group.calendar.google.com&amp;color=%2342104A&amp;ctz=Europe%2FBrussels&dates=20180702%2F20180726" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe> --> */}
             <iframe
               title="osoc calendar"
-              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=en_GB&amp;color=%23B1440E&amp;bgcolor=%23ffffff&amp;src=bWlldGNsYWVzLmNvbV9oMXJzMzFhNzk4MDlyNzQyZTVscGRwZWdiZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;ctz=Europe%2FBrussels&dates=20210705%2F20210730"
+              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=en_GB&amp;color=%23B1440E&amp;bgcolor=%23ffffff&amp;src=bWlldGNsYWVzLmNvbV9oMXJzMzFhNzk4MDlyNzQyZTVscGRwZWdiZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;ctz=Europe%2FBrussels&dates=20220704%2F20220729"
               style={{ borderWidth: 0 }}
               width="100%"
               height="800"
