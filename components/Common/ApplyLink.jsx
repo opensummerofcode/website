5b193116e6f369bc08476dd5b53fe03c91119180
Thor Galle <thorgalle@gmail.com>
2022-06-14 19:03:20 +0300

    Warning page for non-EEA/Swiss applicants

---
+++ components/Common/ApplyLink.jsx
@@ -0,0 +1,71 @@
+import Link from 'next/link';
+import useSWR from 'swr';
+import fetcher from '../../util/fetch';
+import ExternalLink from '../UI/ExternalLink';
+
+const geoJsonFetcher = () => fetcher('http://www.geoplugin.net/json.gp');
+
+export const applicationTallyURL = 'https://tally.so/r/wL8Rvw';
+
+// Generates an "Apply" link based on the location of the visitor.
+// This geolocation info is requested client-side
+//
+// If the visitor is in a EEA/Switzerland country, this link redirects them to
+// the Tally application form.
+
+// If they are outside these countries, it redirects them to a warning page about
+// oSoc not being able to support visas ("non-EU warning").
+//
+// Reason: non-EEA/Swiss students can not work with a "student job contract" in Belgium:
+//
+// https://werk.belgie.be/nl/themas/arbeidsovereenkomsten/bijzondere-arbeidsovereenkomsten/overeenkomst-voor-tewerkstelling-van
+// > "De buitenlandse studenten afkomstig uit een land van de Europese Economische Ruimte
+// >  of uit Zwitserland hebben dezelfde rechten en verplichtingen als de Belgische studenten,
+// >  zelfs als ze geen onderwijs volgen in België of hier niet verblijven."
+const ApplyLink = () => {
+  const { data, error } = useSWR('/geo-info.json', geoJsonFetcher);
+  // TODO: set timeout
+
+  const buttonContent = (
+    <>
+      Apply as a student
+      <span className="button__info">Live in Belgium!</span>
+    </>
+  );
+
+  const validCountryButton = (
+    <ExternalLink href={applicationTallyURL} target="_blank" className="button">
+      {buttonContent}
+    </ExternalLink>
+  );
+
+  const invalidCountryButton = (
+    <Link href="/non-eu-warning">
+      <a className="button">{buttonContent}</a>
+    </Link>
+  );
+
+  // If there is an error, or if the location data is still loading,
+  // then never redirect to the warning page.
+  if (error || !data) {
+    return validCountryButton;
+  }
+
+  if (
+    data.geoplugin_continentCode != null &&
+    data.geoplugin_countryName != null &&
+    !(
+      // EEA are the EU countries + Iceland, Liechtenstein & Norway.
+      (
+        data.geoplugin_continentCode === 'EU' ||
+        /Iceland|Liechtenstein|Norway|Switzerland/.test(data.geoplugin_countryName)
+      )
+    )
+  ) {
+    return invalidCountryButton;
+  }
+
+  return validCountryButton;
+};
+
+export default ApplyLink;
---
+++ pages/non-eu-warning.jsx
@@ -0,0 +1,96 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import useSWR from 'swr';
+import ExternalLink from '../components/UI/ExternalLink';
+import fetcher from '../util/fetch';
+import { applicationTallyURL } from '../components/Common/ApplyLink';
+
+const geoJsonFetcher = () => fetcher('http://www.geoplugin.net/json.gp');
+
+const NonEUWarning = () => {
+  const { data, error } = useSWR('/geo-info.json', geoJsonFetcher);
+
+  let visitorCountry = null;
+  if (!error && !!data && !!data.geoplugin_countryName) {
+    visitorCountry = data.geoplugin_countryName;
+  }
+
+  return (
+    <div className="non-eu-container">
+      <div className="grid-x grid-margin-x">
+        <div className="cell">
+          <Head>
+            <title>Warning | Open Summer of Code</title>
+            {/* TODO: exclude from robots */}
+          </Head>
+          <h1>
+            Warning: oSoc does <span style={{ color: 'red' }}>not support visas </span>
+            <span role="img" aria-label="Sad face">
+              😞
+            </span>
+          </h1>
+          <p>
+            It seems you are visiting us from{' '}
+            {visitorCountry ? (
+              <>
+                <strong>{visitorCountry}</strong>, which is not a country in the European Economic
+                Area or Switzerland.
+              </>
+            ) : (
+              <>outside the EU or Switzerland.</>
+            )}
+          </p>
+
+          <p>
+            <strong>
+              You are probably <span style={{ color: 'red' }}>not</span> eligible to work at oSoc.
+              You will waste your time by starting this application.
+            </strong>
+          </p>
+          <p>We can only process your application if you can prove that:</p>
+          <ul>
+            <li>
+              You are <strong>currently enrolled</strong> in a <strong>full study program</strong>{' '}
+              in <strong>Belgium.</strong>
+            </li>
+            <li>
+              You <strong>currently</strong> have a <strong>valid Belgian residence permit</strong>
+            </li>
+            <li> You are able to work on-location in Brussels, Belgium in July 2022.</li>
+          </ul>
+
+          <div className="go-back-container">
+            <p>
+              <Link href="/">
+                <a className="link link--go-back">I understand, back to home.</a>
+              </Link>
+              <small>
+                <span role="img" aria-label="A tip:">
+                  💡
+                </span>{' '}
+                Follow us on{' '}
+                <ExternalLink className="link--twitter" href="https://twitter.com/osocode">
+                  Twitter
+                </ExternalLink>{' '}
+                to learn when oSoc comes to {visitorCountry || 'your country'}!
+              </small>
+            </p>
+          </div>
+
+          <ExternalLink>
+            <a
+              className="link link--continue"
+              href={applicationTallyURL}
+              target="_blank"
+              rel="noreferrer"
+            >
+              I have the required documents, continue.
+            </a>
+          </ExternalLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NonEUWarning;
