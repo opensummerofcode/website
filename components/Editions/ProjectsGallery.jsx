c6ac02726c3000aa78625285a13b701dd4714e0e
Jonathan Beliën <dev@jbelien.be>
2022-07-13 21:55:46 +0200

    Update ProjectsGallery

--- components/Editions/ProjectsGallery.jsx
+++ components/Editions/ProjectsGallery.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import slugify from 'slugify';
 import Project from './Project';
 
 const Projects = ({ edition, projects, isDemoDay }) => {
-  const $projects = projects.map((p) => (
-    <Project isDemoDay={isDemoDay} key={p.id} edition={edition} project={p} />
-  ));
+  const $projects = projects.map((p) => {
+    const slug = slugify(p.name, { lower: true });
+    return <Project isDemoDay={isDemoDay} key={p.id ?? slug} edition={edition} project={p} />;
+  });
   return <div className="c-projects">{$projects}</div>;
 };
 
